57df61a81e41b9b12bffe5d171e4634f867e23cc
reicen <reicenvalena18@gmail.com>
2023-06-18 05:39:06 +0800

    add multiple assignment.py

---
+++ multiple assignment.py
@@ -0,0 +1,24 @@
+#multiple assignment
+#ex STANDARD:
+#name = "reicen"
+#age = 23
+#ganda_ka = True
+
+#MULTIPLE ASSIGNMENT = SEPARATE THEM W COMMA
+
+name, age, ganda_ka = 'reicen', 23, True
+
+print(name)
+print(age)
+print(ganda_ka)
+
+#EQUAL VALUES
+
+ariana = camila = maja = 1
+
+print(ariana)
+print(camila)
+print(maja)
+
+
+
