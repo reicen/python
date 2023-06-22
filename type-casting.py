46f783a90e81b3bb6b94927eca44825605509095
reicen <reicenvalena18@gmail.com>
2023-06-23 07:40:35 +0800

    Add type-casting.py

---
+++ type-casting.py
@@ -0,0 +1,24 @@
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+print(x)
+print(y)
+print(z)
+
+print(x)
+print(int(y))
+print(z)
+
+x = float(x)
+y = str(y)
+z = int(z)
+
+
+print(x)
+print(y)
+print(z*3)
+
+
+print(y+" joints sksksk")
+#ALWAYS CONVERT VALUES INTO STRINGS PAG MAGPPRINT W OTHER LITERAL STRINGS
