a4ed1482b1d2ffa49f8956222959b851dd86fb23
reicen <reicenvalena18@gmail.com>
2023-06-28 04:11:15 +0800

    Add user-input1.py

---
+++ user-input1.py
@@ -0,0 +1,24 @@
+#USER INPUT - always str datatype
+#input("What is your first name?: ")
+#input("What is your last name?: ")
+
+first_name = input('First Name: ')
+last_name = input('Last Name: ')
+print("Good day, " + first_name+" "+last_name+ "!")
+
+#age = input('Age: ')
+#print(age+" ka na? Tanders yarn???")
+
+#age = int(input('Age: '))
+#print(age*3)
+#print('noice ;)')
+
+age = int(input('Age: '))
+age += 1
+print('mag-'+str(age)+' ka na? Tanders yarn???')
+
+weight = float(input('Weight: '))
+weight += 1
+print ('ah '+str(weight)+', ay xori dinagdagan ko ba? sksk')
+
+input('bye!')
