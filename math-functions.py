bebb38c7d8b7976fdb7c44872fe16d33b5b9a359
reicen <reicenvalena18@gmail.com>
2023-11-17 20:10:16 +0800

    edit math-functions.py

---
+++ math-functions.py
@@ -0,0 +1,18 @@
+import math
+
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(69))
+
+x = 3
+y = 56
+z = 420
+
+print(max(x,y,z))
+print(min(x,y,z))
+
+
