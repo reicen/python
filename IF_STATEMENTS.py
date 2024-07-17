d2be014bdecd8865501a570332c5381d82f188ab
reicen <reicenvalena18@gmail.com>
2024-07-17 18:53:50 +0800

    added if statements

---
+++ IF_STATEMENTS.py
@@ -0,0 +1,11 @@
+#IF statements = a block of code that will execute if its condition is TREW
+
+age = int(input('How old are you? '))
+if age == 69:
+    print('noice')
+elif age >= 18:
+    print('tara inom tayo brad')
+elif age <= 0:
+    print('sabaok')
+else:
+    print('bata ka pa inom ka muna gatas dodong')
